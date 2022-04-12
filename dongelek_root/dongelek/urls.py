f66ad78f07ffd19f36b80c9943427ab5423c4c38
MegaMinInKZ <200103287@stu.sdu.edu.kz>
2022-04-12 15:31:54 +0600

    Fixing the errors

--- dongelek_root/dongelek/urls.py
+++ dongelek_root/dongelek/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('car.urls')),
     path('profile/', include('chat.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
